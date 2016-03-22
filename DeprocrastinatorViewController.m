3f4cb354bbfa843cf214ce01e100ea079e5c64d2
ALIREZA TABRIZI <alirezatabrizi@m.local>
2016-03-22 09:10:56 -0700

    changed edit text to done, enable deletion, also deletion from main page

--- DeprocrastinatorViewController.m
+++ DeprocrastinatorViewController.m
@@ -28,7 +28,9 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - tableView must functions
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
     cell.textLabel.text = [self.toDo objectAtIndex:indexPath.row];
     
@@ -39,12 +41,16 @@
     return self.toDo.count;
 }
 
+#pragma mark
+
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     cell.textLabel.highlightedTextColor = [UIColor greenColor];
     
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
+
+#pragma mark- actions
 
 - (IBAction)onAddButtonPressed:(UIBarButtonItem *)sender {
     //self.textField.text =
@@ -53,19 +59,56 @@
     [self.textFieldLabel resignFirstResponder];
     
 }
-- (IBAction)onEditButtonPressed:(id)sender {
-    //setLeftBarButtonItem:<#(nullable UIBarButtonItem *)#> animated:YES];
-    //self.navigationItem.leftBarButtonItem.title = @"Done";
-
-    if ([self.navigationItem.leftBarButtonItem.title isEqualToString:@"Edit"])
-    {NSLog(@"inside the if statemenet");
-        self.navigationItem.leftBarButtonItem.title= @"Done";
-    }
+- (IBAction)onSwipredRight:(UISwipeGestureRecognizer *)sender {
+   //[]
 }
 
 
-//tableView, canMove
-//tabeView, canEdit
+
+#pragma mark - edit selected, delete and swap rows
+
+- (IBAction)onEditButtonPressed:(id)sender {                ///- (IBAction)onEditButtonPressed:(UIBarButtonItem *)sender {
+    
+    //setLeftBarButtonItem: animated:YES];
+    //self.navigationItem.leftBarButtonItem.title = @"Done";
+        self.navigationItem.leftBarButtonItem.title = @"Edit";                               ///    if (self.editing){
+        if (self.editing) {                                     ///        self.editing = false;
+            self.editing = NO;                                  ///        [self.tableView setEditing:false animated:true];
+            [self.tableView setEditing:NO animated:YES];
+            self.navigationItem.leftBarButtonItem.title = @"Edit";///        sender.style = UIBarButtonItemStylePlain;
+        } else{                                                 ///        sender.title = @"Edit";
+            self.editing = YES;                                 ///    } esle {
+            [self.tableView setEditing:YES animated:YES];
+            self.navigationItem.leftBarButtonItem.title = @"Done";///        self.editing = true;
+        }                                                       ///        [self.tableView setEditing:true animated:true];
+                                                               ///        sender.title = @"Done";
+                                                               ///        sender.style =  UIBarButtonItemStyleDone;
+    [self.tableView reloadData];                                ///    }
+                                                                ///    [self.tableView reloadData];
+}
+
+-(BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath{
+    return YES;
+}
+
+-(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath{
+    
+    NSString *stringToMove = self.toDo[sourceIndexPath.row];
+    [self.toDo removeObjectAtIndex:sourceIndexPath.row];
+    [self.toDo insertObject:stringToMove atIndex:destinationIndexPath.row];
+}
+
+#pragma Mark - swipe and edit/delete
+
+-(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath{
+    return YES;
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    [self.toDo removeObjectAtIndex:indexPath.row];
+    [self.tableView reloadData];
+}
+
 
 -(void)getTextFromTextField
 {
